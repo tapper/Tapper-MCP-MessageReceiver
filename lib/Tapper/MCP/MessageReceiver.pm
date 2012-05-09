77d1524bfb20df7aee91d648529cf3f3d4267910
Steffen Schwigon <steffen.schwigon@amd.com>
2012-05-09 13:31:26 +0200

    Дист::Зилла Roundhouse kick

--- lib/Tapper/MCP/MessageReceiver.pm
+++ lib/Tapper/MCP/MessageReceiver.pm
@@ -1,33 +1,4 @@
 package Tapper::MCP::MessageReceiver;
-
-our $VERSION = '3.000010';
+# ABSTRACT: Tapper - Message receiver for Tapper MCP
 
 1; # End of Tapper::MCP::MessageReceiver
-
-__END__
-
-=head1 NAME
-
-Tapper::MCP::MessageReceiver - Tapper - Message receiver for Tapper MCP
-
-=head1 SYNOPSIS
-
-    $ tapper-mcp-messagereceiver
-
-=head1 AUTHOR
-
-AMD OSRC Tapper Team, C<< <tapper at amd64.org> >>
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Tapper::MCP::MessageReceiver
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2011 AMD OSRC Tapper Team, all rights reserved.
-
-This program is released under the following license: freebsd
-
--- t/00-load.t
+++ t/00-load.t
@@ -5,5 +5,3 @@ use Test::More tests => 1;
 BEGIN {
 	use_ok( 'Tapper::MCP::MessageReceiver' );
 }
-
-diag( "Testing Tapper::MCP::MessageReceiver $Tapper::MCP::MessageReceiver::VERSION, Perl $], $^X" );
